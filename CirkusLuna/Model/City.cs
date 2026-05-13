3bfda0bfcfcab719cc923070cd86c4c510b673f1
Victoria Bruhn <vib006@edu.zealand.dk>
2026-05-13 13:10:40 +0200

    Show + city klasser

--- CirkusLuna/Model/City.cs
+++ CirkusLuna/Model/City.cs
@@ -2,5 +2,15 @@
 {
     public class City
     {
+        //City Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Constructor
+        public City(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
---
+++ CirkusLuna/Model/Show.cs
@@ -0,0 +1,26 @@
+﻿namespace CirkusLuna.Model
+{
+    public class Show
+    {
+        //Show Properties
+        public int Id { get; set; }
+        public string ShowName { get; set; } = string.Empty;
+        public DateOnly Date { get; set; }
+        public int TotalSeats { get; set; }
+        
+        //By hvor forestillingen finder sted
+        public City City { get; set; }
+
+        //Constructor
+        public Show(int id, string showName, DateOnly date, int totalSeats, City city)
+        {
+            Id = id;
+            ShowName = showName;
+            Date = date;
+            TotalSeats = totalSeats;
+            City = city;
+        }
+        
+
+    }
+}
